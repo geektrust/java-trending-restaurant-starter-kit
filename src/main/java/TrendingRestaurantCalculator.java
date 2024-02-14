a5a5fcaea6daf217eb4c551aa76baaf6ce0ffe30
Joydeep Bhattacharya <58521295+linkwithjoydeep@users.noreply.github.com>
2024-02-14 18:39:57 +0530

    return null for identify restaurant by default

--- src/main/java/TrendingRestaurantCalculator.java
+++ src/main/java/TrendingRestaurantCalculator.java
@@ -2,6 +2,6 @@ import java.util.List;
 
 public class TrendingRestaurantCalculator {
     public static Restaurant identifyTrendingRestaurant(List<Restaurant> restaurants) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return null;
     }
 }
